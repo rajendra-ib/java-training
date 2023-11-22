95b3da093c25a10fce94dd4fdef44ef058a50007
Rajendra Prasad Arukala <rajendra.arukala@infiniteblue.com>
2023-11-22 21:44:25 +0530

    Final commit in operators.

--- src/fundamentals/DemoOperators.java
+++ src/fundamentals/DemoOperators.java
@@ -4,6 +4,7 @@ public class DemoOperators {
     public static void main(String[] args) {
         System.out.println("Demo of Operators begin..");
         System.out.println("There are many types of operators in java.");
+        System.out.println("Pretend that Operators Demo is over.");
     }
 }
 
