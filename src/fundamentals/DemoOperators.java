b1606123c6344c4bc6ad6542664f6e869562971f
Rajendra Prasad Arukala <rajendra.arukala@infiniteblue.com>
2023-11-22 20:55:07 +0530

    Added new line for printing help text in a class for Operators

--- src/fundamentals/DemoOperators.java
+++ src/fundamentals/DemoOperators.java
@@ -1,5 +1,6 @@
 public class DemoOperators {
     public static void main(String[] args) {
         System.out.println("Demo of Operators begin..");
+        System.out.println("There are many types of operators in java.")
     }
 }
