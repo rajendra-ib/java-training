2ae9687edbf38d8e0151ceedb406022e816c36c9
Rajendra Prasad Arukala <rajendra.arukala@infiniteblue.com>
2023-11-23 11:50:18 +0530

    The title of the commit ( purpose ) - another commit.

--- src/fundamentals/DemoControlStructures.java
+++ src/fundamentals/DemoControlStructures.java
@@ -4,5 +4,6 @@ public class DemoControlStructures {
     public static void main(String[] args) {
         System.out.println("Demonstration of Control Structures");
         System.out.println("A console statement");
+        System.out.println("THE END");
     }
 }
