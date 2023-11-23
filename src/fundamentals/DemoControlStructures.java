04cc58102a0386109f1c7b41751ca6c79ca1a7e5
Rajendra Prasad Arukala <rajendra.arukala@infiniteblue.com>
2023-11-23 12:22:16 +0530

    Committing for a branch once more.

--- src/fundamentals/DemoControlStructures.java
+++ src/fundamentals/DemoControlStructures.java
@@ -4,5 +4,6 @@ public class DemoControlStructures {
     public static void main(String[] args) {
         System.out.println("Demonstration of Control Structures");
         System.out.println("NEW LINE");
+        System.out.println("ANOTHER NEW LINE");
     }
 }
