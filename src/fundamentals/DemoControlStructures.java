c3fc712fbb29d0f3afefc86fa1c69951b11d4ef8
Rajendra Prasad Arukala <rajendra.arukala@infiniteblue.com>
2023-11-23 11:57:20 +0530

    Committing for a branch

--- src/fundamentals/DemoControlStructures.java
+++ src/fundamentals/DemoControlStructures.java
@@ -3,7 +3,5 @@ package fundamentals;
 public class DemoControlStructures {
     public static void main(String[] args) {
         System.out.println("Demonstration of Control Structures");
-        System.out.println("A console statement");
-        System.out.println("THE END");
     }
 }
